c47c620052e104867fa69e45863100c9f9d7866e
sai chaitanya <csai30185@gmail.com>
2025-05-01 23:51:45 +0530

    some changes in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import React,{ StrictMode } from 'react'
 import { createRoot ,HashRouter} from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
