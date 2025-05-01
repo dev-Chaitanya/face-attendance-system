e6fb79eb01f185139b9620f5cbb4b1a0f5bc662e
sai chaitanya <csai30185@gmail.com>
2025-05-02 02:02:40 +0530

    changes in path in app.jsx of components

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Include Bootstrap JS with
 import 'sweetalert2/dist/sweetalert2.min.css';
 
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Register from "./components/Auth/Register.jsx";
-import Dashboard from "./components/Dasboard.jsx";
-import StudentRegister from "./pages/StudentRegister.jsx";
-import AttendanceRecords from "./pages/AttendanceRecords.jsx";
-import ManageAttendance from "./pages/ManageAttendance.jsx";
-import Records from "./pages/Records.jsx";
+import Home from "./pages/Home";
+import Register from "./components/Auth/Register";
+import Dashboard from "./components/Dasboard";
+import StudentRegister from "./pages/StudentRegister";
+import AttendanceRecords from "./pages/AttendanceRecords";
+import ManageAttendance from "./pages/ManageAttendance";
+import Records from "./pages/Records";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 
