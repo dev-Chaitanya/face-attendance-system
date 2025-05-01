1d150273328c62ae561b6a006c3cec7d9920ea87
sai chaitanya <csai30185@gmail.com>
2025-05-01 23:49:21 +0530

    some changes in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,10 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+
   <HashRouter>
     <App />
   </HashRouter>,
+  </React.StrictMode>
 )
