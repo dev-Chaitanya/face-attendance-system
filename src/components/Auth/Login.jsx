eda5ce8cabcc1445f5821b8992490fc2834ed44d
sai chaitanya <csai30185@gmail.com>
2025-05-02 01:12:28 +0530

    changes added comments in png to check

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // Initialize useNavigate
 
   // Toggle password visibility
   const togglePasswordVisibility = () => {
